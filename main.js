be1e3d30f9950e563e985a0325c1829dcb0b29e7
Rohan Chakravarthy <rohanc219@gmail.com>
2015-10-13 01:31:24 -0400

    Seems Complete - Still don't like the way area code has been handled - too specific

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function main()
 
 	constraints(filePath);
 
-	generateTestCases()
+	generateTestCases(filePath)
 
 }
 
@@ -59,6 +59,14 @@ var functionConstraints =
 
 var mockFileLibrary = 
 {
+	
+  	nonEmptyDir: 
+  	{
+    	'nonEmptyPath':
+    	{
+    		'some-file.txt': 'file content here'
+		}
+	},
 	pathExists:
 	{
 		'path/fileExists': {}
@@ -69,13 +77,20 @@ var mockFileLibrary =
 		{	
   			file1: 'text content',
 		}
+	},
+	fileWithoutContent:
+	{
+		pathContent: 
+		{	
+  			file2: '',
+		}
 	}
 };
 
-function generateTestCases()
+function generateTestCases(fileToTest)
 {
 
-	var content = "var subject = require('./subject.js')\nvar mock = require('mock-fs');\n";
+	var content = "var subject = require('./"+fileToTest+"')\nvar mock = require('mock-fs');\n";
 	for ( var funcName in functionConstraints )
 	{
 		
@@ -97,8 +112,58 @@ function generateTestCases()
 		// update parameter values based on known constraints.
 		var constraints = functionConstraints[funcName].constraints;
 		// Handle global constraints...
+
+		//If true, this will generate a file with some content
 		var fileWithContent = _.some(constraints, {kind: 'fileWithContent' });
-		var pathExists      = _.some(constraints, {kind: 'fileExists' });
+
+		var constraintsOnFileContents = _.filter(constraints, {kind: 'fileWithContent' });
+
+		//all the idents that need a fileWithContent
+		var fileWithContentList = []
+		if(constraintsOnFileContents.length)
+			constraintsOnFileContents.forEach(function(constraint){
+				if(constraint.ident)
+				{
+					fileWithContentList.push(constraint.ident);
+				}
+			})
+
+		//If true, this will generate an empty directory
+		var pathExists      = _.some(constraints, {kind: 'fileOrDirExists' });
+
+		var constraintsOnFileOrDirExists = _.filter(constraints, {kind: 'fileOrDirExists' });
+		
+		//all the idents that can be satisifed by either a file or a directory
+		var fileOrDirExistsList = []
+		if(constraintsOnFileOrDirExists.length)
+			constraintsOnFileOrDirExists.forEach(function(constraint){
+				if(constraint.ident)
+				{
+					fileOrDirExistsList.push(constraint.ident);
+				}
+			})
+
+		var constraintOnObject = _.filter(constraints, {kind: 'object' });
+		var objectList = []
+		if(constraintOnObject.length)
+			constraintOnObject.forEach(function(constraint){
+				if(constraint.ident)
+				{
+					objectList.push(constraint.ident);
+				}
+			})
+
+		var constraintOnBoolean = _.filter(constraints, {kind: 'boolean' });
+		var booleanList = []
+		if(constraintOnBoolean.length)
+			constraintOnBoolean.forEach(function(constraint){
+				if(constraint.ident)
+				{
+					booleanList.push(constraint.ident);
+				}
+			})
+
+		var non_function_variables_indexes = []
 
 		// plug-in values for parameters
 		for( var c = 0; c < constraints.length; c++ )
@@ -106,10 +171,67 @@ function generateTestCases()
 			var constraint = constraints[c];
 			if( params.hasOwnProperty( constraint.ident ) )
 			{
+				//Now for all those idents which need fileOrDir but also need a FileWithContent, change their value to a file
+				if(_.contains(fileWithContentList, constraint.ident) && _.contains(fileOrDirExistsList, constraint.ident))
+				{
+					if (constraint.value == "'path/fileExists'")
+						continue;
+				}
+
+				if(_.contains(objectList, constraint.ident) && _.contains(booleanList, constraint.ident))
+				{
+					if (constraint.kind=="boolean")
+						continue;
+				}
+
 				params[constraint.ident].push(constraint.value);
 				//params[constraint.ident] = (constraint.value);
 			}
+
+			else{
+				//This is a contraint on a variable that is not a function parameter
+				//Lets just assign the function parameter this contraint value
+				//Assuming the variable is derived from a function parameter, this might just work
+				non_function_variables_indexes.push(c);
+			}
 		}
+		
+		for(var i=0;i<non_function_variables_indexes.length;i++)
+		{
+			constraint = constraints[i];
+
+			for (var property in params) {
+			    if (params.hasOwnProperty(property)) {
+			    	var dummy_value = faker.phone.phoneNumber(faker.definitions.phone_number.formats[0]);
+			        var value_list = params[property];
+			        if(value_list.length)
+			        {
+			        	var first_value = value_list[0];
+			        	if(typeof(first_value)!='string')
+			        		continue;
+			        }
+			        else{
+			        	params[property].push("\""+dummy_value+"\"");
+			        	//This is going be assigned a phone number Anyway
+			        	//Hardcoding replacing area code. This pains me. Need to go through the faker phonenumber code to fix this
+			        	var len = constraint.value.length;
+			        	// if(len <=dummy_value.length)
+			        	// {
+		        		// console.log("Actual Value:"+constraint.value);
+		        		// console.log("Before:"+dummy_value);
+		        		dummy_value = constraint.value + dummy_value.substring(len);
+		        		// console.log("After:"+dummy_value);
+			        	// }
+			        	// else{
+			        	// 	dummy_value = constraint.value;
+			        	// }
+			        	params[property].push("\""+dummy_value+"\"");
+			        }
+			        
+			    }
+			}
+		}
+
 
 		var arg_combinations = generateCombinations(params);
 
@@ -117,11 +239,11 @@ function generateTestCases()
 
 			if( pathExists || fileWithContent )
 			{
-				content += generateMockFsTestCases(pathExists,fileWithContent,funcName, args);
+				content += generateMockFsTestCases(pathExists,fileWithContent,!fileWithContent,true, funcName, args);
 				// Bonus...generate constraint variations test cases....
-				content += generateMockFsTestCases(!pathExists,fileWithContent,funcName, args);
-				content += generateMockFsTestCases(pathExists,!fileWithContent,funcName, args);
-				content += generateMockFsTestCases(!pathExists,!fileWithContent,funcName, args);
+				content += generateMockFsTestCases(!pathExists,fileWithContent,!fileWithContent,false, funcName, args);
+				content += generateMockFsTestCases(pathExists,!fileWithContent,fileWithContent,true, funcName, args);
+				content += generateMockFsTestCases(!pathExists,!fileWithContent,fileWithContent,false, funcName, args);
 			}
 			else
 			{
@@ -139,6 +261,8 @@ function generateCombinations(args_map)
 {
 	key_list = []
 
+	// console.log(args_map);
+
 	for (var key in args_map) {
 		key_list.push(key);
 		
@@ -153,6 +277,13 @@ function generateCombinations(args_map)
 		queue.push(first_key_values[i]);
 	}
 
+	var default_param = "'"+faker.phone.phoneNumber()+"'";
+
+	if(queue.length==0)
+	{
+		queue.push(default_param);
+	}
+
 
 	for(var i=1;i<key_list.length;i++)
 	{
@@ -162,25 +293,41 @@ function generateCombinations(args_map)
 		curr_key = key_list[i];
 		curr_vals = args_map[curr_key];
 
+		if(curr_vals.length == 0)
+		{
+			curr_vals.push(default_param);
+		}
+
 		while(old_q.length>0)
 		{
 			str = old_q.shift();
 
 			if(typeof(str)!='string')
 			{
-				str = str.toString();
+				if(typeof(str)=='object')
+				{
+					str = JSON.stringify(str);
+				}
+				else
+				{
+					str = str.toString();
+				}
 			}
 
 			for(var j=0;j<curr_vals.length;j++)
 			{
 				temp_str = str.slice(0);
-				temp_str += ',' + curr_vals[j];
+				var next_val = curr_vals[j];
+				if(typeof(next_val)=='object')
+				{
+					next_val = JSON.stringify(next_val);
+				}
+				temp_str += ',' + next_val;
 				queue.push(temp_str);
 			}
 		}
 
 	}
-	var default_param = "'"+faker.phone.phoneNumber()+"'";
 	if (queue.length == 0)
 	{
 		str = default_param;
@@ -191,11 +338,12 @@ function generateCombinations(args_map)
 		queue.push(str);
 
 	}
+	// console.log(queue);
 	return queue;
 
 }
 
-function generateMockFsTestCases (pathExists,fileWithContent,funcName,args) 
+function generateMockFsTestCases (pathExists,fileWithContent,fileWithoutContent,nonEmptyDir, funcName,args) 
 {
 	var testCase = "";
 	// Build mock file system based on constraints.
@@ -207,6 +355,15 @@ function generateMockFsTestCases (pathExists,fileWithContent,funcName,args)
 	if( fileWithContent )
 	{
 		for (var attrname in mockFileLibrary.fileWithContent) { mergedFS[attrname] = mockFileLibrary.fileWithContent[attrname]; }
+	}
+
+	if( fileWithoutContent )
+	{
+		for (var attrname in mockFileLibrary.fileWithoutContent) { mergedFS[attrname] = mockFileLibrary.fileWithoutContent[attrname]; }
+	}
+	if ( nonEmptyDir)
+	{
+		for (var attrname in mockFileLibrary.nonEmptyDir) { mergedFS[attrname] = mockFileLibrary.nonEmptyDir[attrname]; }
 	}
 
 	testCase += 
@@ -241,51 +398,85 @@ function constraints(filePath)
 			{
 				if( child.type === 'BinaryExpression' && child.operator == "==")
 				{
-					if( child.left.type == 'Identifier' && params.indexOf( child.left.name ) > -1)
+					if( child.left.type == 'Identifier')
 					{
 						// get expression from original source code:
 						var expression = buf.substring(child.range[0], child.range[1]);
 						var rightHand = buf.substring(child.right.range[0], child.right.range[1])
 
-						functionConstraints[funcName].constraints.push( 
-							new Constraint(
-							{
-								ident: child.left.name,
-								value: rightHand,
-								funcName: funcName,
-								kind: "integer",
-								operator : child.operator,
-								expression: expression
-							}));
-						if(!isNaN(parseFloat(rightHand))){
-							functionConstraints[funcName].constraints.push( 
-								new Constraint(
-								{
-									ident: child.left.name,
-									value: parseFloat(rightHand) + 1,
-									funcName: funcName,
-									kind: "integer",
-									operator : child.operator,
-									expression: expression
-								}));
 
-							functionConstraints[funcName].constraints.push( 
-								new Constraint(
-								{
-									ident: child.left.name,
-									value: parseFloat(rightHand) -1,
-									funcName: funcName,
-									kind: "integer",
-									operator : child.operator,
-									expression: expression
-								}));
+						if(params.indexOf( child.left.name ) > -1)
+						{
+							if(!isNaN(parseInt(rightHand))){
+								functionConstraints[funcName].constraints.push( 
+									new Constraint(
+									{
+										ident: child.left.name,
+										value: rightHand,
+										funcName: funcName,
+										kind: "integer",
+										operator : child.operator,
+										expression: expression
+									}));
+							}
+							else if(!isNaN(parseFloat(rightHand))){
+								functionConstraints[funcName].constraints.push( 
+									new Constraint(
+									{
+										ident: child.left.name,
+										value: parseFloat(rightHand) + 1,
+										funcName: funcName,
+										kind: "integer",
+										operator : child.operator,
+										expression: expression
+									}));
+
+								functionConstraints[funcName].constraints.push( 
+									new Constraint(
+									{
+										ident: child.left.name,
+										value: parseFloat(rightHand) -1,
+										funcName: funcName,
+										kind: "integer",
+										operator : child.operator,
+										expression: expression
+									}));
+							}
+							else{
+								functionConstraints[funcName].constraints.push( 
+									new Constraint(
+									{
+										ident: child.left.name,
+										value: "\"" + rightHand.replace(/['"]+/g, '').concat("1") + "\"",
+										funcName: funcName,
+										kind: "string",
+										operator : child.operator,
+										expression: expression
+									}));
+								
+								functionConstraints[funcName].constraints.push( 
+									new Constraint(
+									{
+										ident: child.left.name,
+										value: rightHand,
+										funcName: funcName,
+										kind: "string",
+										operator : child.operator,
+										expression: expression
+									}));
+							}
 						}
+
 						else{
+							//This is not a function parameter
+							//This is a contraint on a variable that is not a function parameter
+							//Lets just assign the function parameter this contraint value
+							//Assuming the variable is derived from a function parameter, this might just work
 							functionConstraints[funcName].constraints.push( 
 								new Constraint(
 								{
 									ident: child.left.name,
-									value: "\"" + rightHand.replace(/['"]+/g, '').concat("1") + "\"",
+									value: rightHand.replace(/['"]+/g, ''),
 									funcName: funcName,
 									kind: "string",
 									operator : child.operator,
@@ -362,6 +553,72 @@ function constraints(filePath)
 
 				}
 
+				else if( child.type === 'UnaryExpression' && child.operator=="!")
+				{
+					// console.log(child);
+					if(child.argument.type == 'Identifier')
+					{
+						// console.log(child.argument.name);
+						functionConstraints[funcName].constraints.push( 
+							new Constraint(
+							{
+								ident: child.argument.name,
+								value: true,
+								funcName: funcName,
+								kind: "boolean",
+								operator : child.operator,
+								expression: expression
+							}));
+						functionConstraints[funcName].constraints.push( 
+							new Constraint(
+							{
+								ident: child.argument.name,
+								value: false,
+								funcName: funcName,
+								kind: "boolean",
+								operator : child.operator,
+								expression: expression
+							}));
+					}
+
+					else if(child.argument.type == 'MemberExpression')
+					{
+						if(child.argument.object)
+						{
+							if(child.argument.property)
+							{
+								var ident_val = child.argument.property.name
+								var obj1 = new Object;
+								obj1[ident_val] = true;
+								var obj2 = new Object;
+								obj2[ident_val] = false;
+								functionConstraints[funcName].constraints.push( 
+								new Constraint(
+								{
+									ident: child.argument.object.name,
+									value: obj1,
+									funcName: funcName,
+									kind: "object",
+									operator : child.operator,
+									expression: expression
+								}));
+
+								functionConstraints[funcName].constraints.push( 
+								new Constraint(
+								{
+									ident: child.argument.object.name,
+									value: obj2,
+									funcName: funcName,
+									kind: "object",
+									operator : child.operator,
+									expression: expression
+								}));
+							}
+						}
+					}
+
+				}
+
 
 				if( child.type == "CallExpression" && 
 					 child.callee.property &&
@@ -378,6 +635,18 @@ function constraints(filePath)
 								value:  "'pathContent/file1'",
 								funcName: funcName,
 								kind: "fileWithContent",
+								operator : child.operator,
+								expression: expression
+							}));
+
+							//also one without any data
+							functionConstraints[funcName].constraints.push( 
+							new Constraint(
+							{
+								ident: params[p],
+								value:  "'pathContent/file2'",
+								funcName: funcName,
+								kind: "fileWithoutContent",
 								operator : child.operator,
 								expression: expression
 							}));
@@ -433,7 +702,30 @@ function constraints(filePath)
 								// A fake path to a file
 								value:  "'path/fileExists'",
 								funcName: funcName,
-								kind: "fileExists",
+								kind: "fileOrDirExists",
+								operator : child.operator,
+								expression: expression
+							}));
+						}
+					}
+				}
+				
+				if( child.type == "CallExpression" &&
+					 child.callee.property &&
+					 child.callee.property.name =="readdirSync")
+				{
+					for( var p =0; p < params.length; p++ )
+					{
+						if( child.arguments[0].name == params[p] )
+						{
+							functionConstraints[funcName].constraints.push( 
+							new Constraint(
+							{
+								ident: params[p],
+								// A fake path to a file
+								value:  "'nonEmptyPath'",
+								funcName: funcName,
+								kind: "nonEmptyDirExists",
 								operator : child.operator,
 								expression: expression
 							}));
